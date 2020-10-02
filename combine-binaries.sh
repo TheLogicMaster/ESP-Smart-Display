0fda977b27fbb730d28dea3846997e1739005e90
TheLogicMaster <justinmarentette11@gmail.com>
2020-10-02 16:13:13 -0400

    Fixed binary merge commands and started ESP32 work

--- combine-binaries.sh
+++ combine-binaries.sh
@@ -3,6 +3,6 @@
 # Combine FS and Firmware binaries to create flashable binary
 
 firmwareSize=$(wc -c $1 | awk '{print $1}')
-fallocate -l $(expr 1052688 + 1044464 - $firmwareSize) ./empty.bin
+fallocate -l $(expr 1052688 + 1044464 - $firmwareSize) ./empty.bin # Values from eagle file
 cat $1 ./empty.bin $2 > $3
 rm ./empty.bin
