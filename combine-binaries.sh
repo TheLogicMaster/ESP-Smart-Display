f8f9d23797656200a291bc45b69569eb1b86c2a2
TheLogicMaster <justinmarentette11@gmail.com>
2020-10-02 14:47:39 -0400

    Forgot to add script

---
+++ combine-binaries.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ 
+# Combine FS and Firmware binaries to create flashable binary
+
+firmwareSize=$(wc -c $1 | awk '{print $1}')
+fallocate -l $(expr 1052688 + 1044464 - $firmwareSize) ./empty.bin
+cat $1 ./empty.bin $2 > $3
+rm ./empty.bin
